6d81d2a736755e4df9b986a021624e0f3ea19d84
francine <francinebc.mail@gmail.com>
2020-04-11 14:14:40 +1200

    add bootstrap

---
+++ ClubSite/Controllers/RandomController.cs
@@ -0,0 +1,17 @@
+﻿using ClubSite.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClubSite.Controllers
+{
+    [Route("api/random/[action]")]
+    public class RandomController : Controller
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            // return Ok("hello");
+            return Unauthorized("nauuuugggghhhhtttyyyy");
+        }
+    }
+}
